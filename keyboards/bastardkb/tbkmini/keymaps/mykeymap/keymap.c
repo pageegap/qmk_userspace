97e794265ce122c36c04b21eaeaca91713560a27
eduardo pacheco <eduardopacheco@eduardos-MacBook-Pro.local>
2024-09-25 20:19:09 -0600

    fix2

--- keyboards/bastardkb/tbkmini/keymaps/mykeymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/mykeymap/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
           KC_BSPC, LCMD_T(KC_A), LOPT_T(KC_S), CTL_T(KC_D), KC_F, KC_G,                  KC_H, RCTL_T(KC_K),ROPT_T(KC_L),RCMD_T(KC_SCLN), KC_QUOT,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-          KC_LSFT, LT(_LOWER, KC_Z), KC_X, KC_C, KC_V, LT(_MOVE, KC_B),                 KC_N,   KC_M,   KC_COMM, KC_DOT, LT(_RAISE, KC_SLSH), KC_RSFT,
+          KC_LSFT, LT(_LOWER, KC_Z), KC_X, KC_C, KC_V,  KC_B,                 KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
         KC_LGUI, KC_SPC, MO(1), MO(2), KC_ENT, KC_RALT
         //`--------------------------'  `--------------------------'
