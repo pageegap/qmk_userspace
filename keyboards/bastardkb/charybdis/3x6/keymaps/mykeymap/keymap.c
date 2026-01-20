a87a592bf64f4bdc0aee6a0b68e362a41970dd54
eduardo <epacheco2285@gmail.com>
2026-01-19 21:55:10 -0600

    reversed

--- keyboards/bastardkb/charybdis/3x6/keymaps/mykeymap/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/mykeymap/keymap.c
@@ -50,15 +50,6 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 // my modifications
 
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] = { ENCODER_CCW_CW(UG_HUED, UG_HUEU),  ENCODER_CCW_CW(UG_SATD, UG_SATU)  },
-    [2] = { ENCODER_CCW_CW(UG_VALD, UG_VALU),  ENCODER_CCW_CW(UG_SPDD, UG_SPDU)  },
-    [3] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
-};
-#endif
-
 enum custom_keycodes {
     NEWLINE = SAFE_RANGE,
     PRINT_SCR
