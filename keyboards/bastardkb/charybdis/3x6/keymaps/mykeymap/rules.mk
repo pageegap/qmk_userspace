9d759ca2f7115fa5c8378c9b012559843f268d9f
eduardo <epacheco2285@gmail.com>
2026-01-19 21:31:26 -0600

    changed target

--- keyboards/bastardkb/charybdis/3x6/keymaps/mykeymap/rules.mk
+++ keyboards/bastardkb/charybdis/3x6/keymaps/mykeymap/rules.mk
@@ -3,9 +3,5 @@ TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
 SEND_STRING_ENABLE = yes
 # testing encoder function
-ifneq ($(strip $(MCU)), atmega32u4)
-ifneq ($(strip $(CONVERT_TO)), stemcell)
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
-endif
-endif
